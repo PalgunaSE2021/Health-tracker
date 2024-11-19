74a84889bd83e445b6b94607056beafd945416e7
Palguna M S <palgunamanchale@gmail.com>
2024-11-19 10:53:15 +0530

    added unit testing for service

--- src/app/services/workout-tracker.service.spec.ts
+++ src/app/services/workout-tracker.service.spec.ts
@@ -13,4 +13,9 @@ describe('WorkoutTrackerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return an empty array when no workouts are stored', () => {
+    const workouts = service.loadWorkouts();
+    expect(workouts).toEqual([]);
+  });
 });
